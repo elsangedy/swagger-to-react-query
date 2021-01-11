7f6f71be49d06e932ed97f1709f2d2364af8b6a1
Jean Carlos Londero <jeanlondero@alunos.utfpr.edu.br>
2021-01-11 17:37:58 -0300

    fix handle query params in requestFn

--- src/generator.js
+++ src/generator.js
@@ -146,8 +146,6 @@ const requestFn = async ({ url, method, pathParams, queryParams, ...rest }) => {
 
   if (urlPathParams) {
     url = urlPathParams.reduce((acc, param) => acc.replace(param, pathParams[param.replace(/{|}/g, '')]), url)
-  } else {
-    queryParams = pathParams
   }
 
   if (url.charAt(0) === '/') {
